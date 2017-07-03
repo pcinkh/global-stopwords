2434ae60b8166523ecd751ee2a49fa77cd1b93e2
pcinkh <pcinkh@gmail.com>
2017-07-04 02:20:08 +0300

    Update settings and version.

--- global_stopwords/settings.py
+++ global_stopwords/settings.py
@@ -8,10 +8,14 @@ __version__ = '0.0.1'
 
 CACHE_FILE = os.path.join(
     tempfile.gettempdir(),
-    'global_stopwords.json',
+    'global_stopwords_{version}.json'.format(version=__version__),
 )
 
-STOPWORDS_URL = 'https://raw.githubusercontent.com/6/stopwords-json/master/stopwords-all.json'  # noqa
+STOPWORDS_VERSION = 'v1.2.0'
+
+STOPWORDS_URL = 'https://raw.githubusercontent.com/6/stopwords-json/{stopwords_version}/stopwords-all.json'.format(  # noqa
+    stopwords_version=STOPWORDS_VERSION,
+)
 
 HTTP_DELAY = 5
 
